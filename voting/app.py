ad7af344484787094b99cf3f83e0737ea3c93130
Ubuntu <labuser@v111node0-f2798c7f0d7744d29ff74b3a6f1d91f0-4.v111node0-f2798c7f0d7744d29ff74b3a6f1d91f0-4.d5.internal.cloudapp.net>
2016-06-21 22:09:23 +0000

    2

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Futbol"
-optionB = "Soccer"
+optionA = "Futbollll"
+optionB = "Soccerrrrr"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
